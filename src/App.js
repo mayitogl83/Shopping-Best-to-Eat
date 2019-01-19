01ec1105039a30f805f7defc8e1e9d966cc0b42c
Mario Gonzalez <mayitogl83@gmail.com>
2019-01-18 19:59:26 -0600

    Add content for app main page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import MainPage from './MainPage';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    products: [
+      {
+        "id": "Cheeses",
+        "title": "FRENCH BRIENS CHEESE",
+        "description": "Describe your service here for a newborn session. What makes it great? Use short catchy text to tell people what you offer, and the benefits they will receive. A great description gets readers in the mood, and makes them more likely to go ahead and book.",
+        "info": "1 hr | Photo Session",
+        "price": "6.99",
+        "qty": "50",
+        "class": 'cheese-img',
+        "mainclass": "exmpl-one"
+      },
+      {
+        "id": "Bacon",
+        "title": "SMOKED VIRGINIA STEAK",
+        "description": "Describe your service here a for studio session. What makes it great? Use short catchy text to tell people what you offer, and the benefits they will receive. A great description gets readers in the mood, and makes them more likely to go ahead and book.",
+        "info": "1/30 hr | Photo Session",
+        "price": "12.99",
+        "qty": "60",
+        "class": "steak-img",
+        "mainclass": "exmpl-two"
+      },
+      {
+        "id": "Olives",
+        "title": "GREEK KALAMATA PEPPERS",
+        "description": "Describe your service here for a couple session. What makes it great? Use short catchy text to tell people what you offer, and the benefits they will receive. A great description gets readers in the mood, and makes them more likely to go ahead and book.",
+        "info": "2 hrs | Photo Session",
+        "price": "8.99",
+        "qty": "80",
+        "class": "pepper-img",
+        "mainclass": "exmpl-three"
+      }
+    ]
+  }
 
   w3_open = ()=> {
   document.getElementById("my-sidebar").setAttribute('class', 'showup' );
@@ -49,12 +85,7 @@ class App extends Component {
             <Link to='' tabIndex="-1">
               <button id="w3-bar-item" className="w3-bar-item"
                 onClick={ (event) => { this.w3_close();
-              }}>Meats</button>
-            </Link>
-            <Link to='' tabIndex="-1">
-              <button id="w3-bar-item" className="w3-bar-item"
-                onClick={ (event) => { this.w3_close();
-              }}>Cheeses</button>
+              }}>Products</button>
             </Link>
             <Link to='./bookonline' tabIndex="-1">
               <button id="w3-bar-item" className="w3-bar-item"
@@ -62,6 +93,36 @@ class App extends Component {
               }}>Find Us</button>
             </Link>
         </div>
+      </div>
+      <div id="navbar-desktop" className="navbar-desktop">
+        <Link to='./' tabIndex="-1">
+          <button className="item"
+            onClick={ (event) => { this.w3_close(); }}
+            >Home</button>
+        </Link>
+        <Link to='./' tabIndex="-1">
+          <button className="item">About</button>
+        </Link>
+        <Link to='./' tabIndex="-1">
+          <button className="item">Products</button>
+        </Link>
+        <Link to='./bookonline' tabIndex="-1">
+          <button className="item">Find Us</button>
+        </Link>
+        <div className="topnav-right">
+        <Link to='./bookonline' tabIndex="-1">
+          <div className="cart-container">
+            <button className="cart-icon"></button>
+          </div>
+        </Link>
+      </div>
+      </div>
+      <div id="page-content" className="content">
+        <Route exact path='/' render={()=>(
+          <MainPage
+            products={ this.state.products }
+          />
+        ) }/>
       </div>
       </div>
     );
---
+++ src/MainPage.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './App.css';
+
+class MainPage extends Component {
+  render() {
+    return(
+    <div className="container">
+      <div className="bgimg"></div>
+    <div className="fade-in one">
+      <div className="logo-container">
+        <div className="title-logo">
+           <div className="logo-text">Best</div>
+           <div className="logo-text-dif">to</div>
+           <div className="logo-text">Eat</div>
+        </div>
+        <div className="logo-adjustment">
+          <div className="under-title-logo">Savouries and sweets for the gourmet in all of us</div>
+          <div className="logo-field">
+          <div className="logo"></div>
+          </div>
+          <div id="start-field" className="start-field">
+          <Link to='./bookonline' tabIndex="-1">
+            <button className="start-button">Start Shooping ></button>
+          </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="product-container">
+      <div className="product-title">Enjoy Our Special Recommendations</div>
+      <div className="product-field">
+            {this.props.products.map((product) => (
+              <Link key={product.id} to="./bookingform" tabIndex="-1">
+              <button className="product-session" tabIndex="-1">
+                <div className={product.class}/>
+                <div className="product-info">
+                  <div className="product-name">{product.title}</div>
+                  <div className="product-price">Price: ${product.price}</div>
+                  <div className="line"></div>
+                </div>
+                <div className="long-line"></div>
+              </button>
+              </Link>
+            ))}
+          </div>
+    </div>
+    </div>
+    )
+  }
+}
+
+export default MainPage
